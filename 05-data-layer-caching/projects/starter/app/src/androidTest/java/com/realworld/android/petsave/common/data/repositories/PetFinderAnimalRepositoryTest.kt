3d7eb8bf7f26feb53690cb968c04015cb89e31c4
LIMBACHIYA DHRUV <dhruvlimbachiya96870@gmail.com>
2022-10-06 11:49:40 +0530

    implemented request more animals method

---
+++ 05-data-layer-caching/projects/starter/app/src/androidTest/java/com/realworld/android/petsave/common/data/repositories/PetFinderAnimalRepositoryTest.kt
@@ -0,0 +1,9 @@
+package com.realworld.android.petsave.common.data.repositories
+
+
+/**
+ * Created by Dhruv Limbachiya on 03-10-2022.
+ */
+class PetFinderAnimalRepositoryTest {
+
+}
--- 05-data-layer-caching/projects/starter/app/src/main/java/com/realworld/android/petsave/common/data/repositories/PetFinderAnimalRepository.kt
+++ 05-data-layer-caching/projects/starter/app/src/main/java/com/realworld/android/petsave/common/data/repositories/PetFinderAnimalRepository.kt
@@ -1,5 +1,8 @@
 package com.realworld.android.petsave.common.data.repositories
 
+import com.realworld.android.petsave.common.data.api.PetFinderApi
+import com.realworld.android.petsave.common.data.api.model.mappers.ApiAnimalMapper
+import com.realworld.android.petsave.common.data.api.model.mappers.ApiPaginationMapper
 import com.realworld.android.petsave.common.data.cache.Cache
 import com.realworld.android.petsave.common.data.cache.model.cachedanimal.CachedAnimalAggregate
 import com.realworld.android.petsave.common.data.cache.model.cachedorganization.CachedOrganization
@@ -14,8 +17,14 @@ import javax.inject.Inject
  * Created by Dhruv Limbachiya on 03-10-2022.
  */
 class PetFinderAnimalRepository @Inject constructor(
-    private val cache: Cache
+    private val cache: Cache,
+    private val petFinderApi: PetFinderApi,
+    private val apiAnimalMapper: ApiAnimalMapper,
+    private val apiPaginationMapper: ApiPaginationMapper
 ) : AnimalRepository {
+
+    private val postalCode = "07097"
+    private val maxDistanceInMiles = 100 // 3
 
     override fun getAnimals(): Flowable<List<Animal>> {
         return cache.getNearbyAnimals().map { animalList ->
@@ -46,7 +55,19 @@ class PetFinderAnimalRepository @Inject constructor(
     }
 
     override suspend fun requestMoreAnimals(pageToLoad: Int, numberOfItems: Int): PaginatedAnimals {
-        TODO("Not yet implemented")
+        val (apiAnimals, apiPagination) = petFinderApi.getNearbyAnimals(
+            pageToLoad,
+            numberOfItems,
+            postalCode,
+            maxDistanceInMiles
+        )
+
+        return PaginatedAnimals(
+            apiAnimals?.map {
+                apiAnimalMapper.mapToDomain(it)
+            } ?: emptyList(),
+            apiPaginationMapper.mapToDomain(apiPagination)
+        )
     }
 
 }
